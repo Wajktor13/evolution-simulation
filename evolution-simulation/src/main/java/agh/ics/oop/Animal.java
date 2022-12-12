4ee07845b515a5093eb8d30720579c77d34da91f
Wajktor13 <wiktorwilkusz13@gmail.com>
2022-12-12 10:36:59 +0100

    add basic Animal fields and getters

--- evolution-simulation/src/main/java/agh/ics/oop/Animal.java
+++ evolution-simulation/src/main/java/agh/ics/oop/Animal.java
@@ -1,8 +1,23 @@
 package agh.ics.oop;
 
 public class Animal extends AbstractWorldMapElement{
-    private short Orientation;
+    private MapDirection orientation;
+    private int age = 0;
+    private int childCounter = 0;
+
     public Animal(Vector2d position){
         super(position);
+    }
+
+    public MapDirection getOrientation(){
+        return this.orientation;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public int getChildCounter(){
+        return this.childCounter;
     }
 }
