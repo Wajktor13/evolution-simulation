848982aa6860bd6bab024670675524e988214db0
Wajktor13 <wiktorwilkusz13@gmail.com>
2022-12-15 12:26:29 +0100

    add plants spawning logic

--- evolution-simulation/src/main/java/agh/ics/oop/EquatorPlantsSpawner.java
+++ evolution-simulation/src/main/java/agh/ics/oop/EquatorPlantsSpawner.java
@@ -1,9 +1,40 @@
 package agh.ics.oop;
 
-public class EquatorPlantsSpawner implements IPlantsSpawner{
+import java.util.ArrayList;
+import java.util.Random;
+
+public class EquatorPlantsSpawner implements IPlantsSpawner {
+    private int radiusY;
+    private int centerY;
+    private Random random = new Random();
+
+    public EquatorPlantsSpawner(Vector2d lowerLeft, Vector2d upperRight, int equatorHeight) {
+        this.radiusY = Math.floorDiv(equatorHeight, 2);
+        this.centerY = Math.floorDiv(upperRight.y - lowerLeft.y, 2);
+    }
 
     @Override
-    public boolean spawn() {
-        return true;
+    public Vector2d generatePlantPosition(ArrayList<Vector2d> freePositions) {
+        float selectArea = random.nextFloat();
+        ArrayList<Vector2d> positionsToSelectFrom = new ArrayList<>();
+
+        if (selectArea <= 0.2) {
+            for (Vector2d position : freePositions) {
+                if (!position.yBetween(new Vector2d(0, this.centerY - this.radiusY),
+                        new Vector2d(0, this.centerY + radiusY))) {
+                    positionsToSelectFrom.add(position);
+                }
+            }
+        } else {
+            for (Vector2d position : freePositions) {
+                if (position.yBetween(new Vector2d(0, this.centerY - this.radiusY),
+                        new Vector2d(0, this.centerY + radiusY))) {
+                    positionsToSelectFrom.add(position);
+                }
+            }
+        }
+
+        return positionsToSelectFrom.get(random.nextInt(0, positionsToSelectFrom.size()));
+
     }
 }
--- evolution-simulation/src/main/java/agh/ics/oop/IPlantsSpawner.java
+++ evolution-simulation/src/main/java/agh/ics/oop/IPlantsSpawner.java
@@ -1,6 +1,17 @@
 package agh.ics.oop;
 
+import java.util.ArrayList;
+
 public interface IPlantsSpawner {
 
-    boolean spawn();
+    /**
+     * Generates a position for a new plant
+     *
+     * @param freePositions
+     *            Currently available positions
+     * @return position for a new plant or null if
+     * if there are no free positions.
+     */
+
+    Vector2d generatePlantPosition(ArrayList<Vector2d> freePositions);
 }
